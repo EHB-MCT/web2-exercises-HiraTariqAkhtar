7613e6ad05a1694cf9afb7c613a8f13af0ea12fb
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2021-10-05 11:35:38 +0200

    fetched pokemons and displayed name, photo and id

---
+++ session2/s2.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let list = [];
+let pokemon = [];
+
+function fetchPokemons(){
+    // get the list
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then(res => res.json())
+    .then(data => {
+       list = data.results;
+       //loop over the list to get each pokemon
+       for(let element of list){
+           fetch(element.url)
+           .then(response => response.json())
+           .then(data => {
+               pokemon.push(data);
+               console.log(pokemon);
+           })
+       }
+    })
+};  
+
+window.onload = function(){
+    fetchPokemons()
+    setTimeout(buildList, 5000);
+    function buildList(){
+        pokemon.sort((a,b) => a.id - b.id);
+        pokemon.forEach(p => {
+            console.log(p);
+            let html = `<div class = "card" id = "${p.id}">
+                         <img src = "${p.sprites.front_default}">
+                         <p> nr.${p.id}
+                         <p> ${p.forms[0].name} </p>
+                         </div>`
+            document.getElementById("list").insertAdjacentHTML("beforeend", html)
+        })
+    }
+}
