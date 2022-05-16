f1ca8c5083980435d8410cdb43f0bdf212b58aee
HiraTariqAkhtar <hira.akhtar@student.ehb.be>
2022-05-16 16:28:02 +0200

    Course project api test

--- Technical_test/apiTest.js
+++ Technical_test/apiTest.js
@@ -7,13 +7,13 @@ async function fetchSerial (){
     .then (data => {
         console.log(data);
         let details = data.results
-        let filtered = details.filter(pic => pic.poster_path != null && pic.first_air_date != "")
+        let filtered = details.filter(pic => pic.poster_path != null && pic.first_air_date != "" && pic.overview != "")
         filtered.forEach(serial =>{
-            let html = `
-            <h2> ${serial.name}</h2>
-            <p> First episode : ${serial.first_air_date} </p>
-            <img src="https://image.tmdb.org/t/p/w200${serial.poster_path}">
-            `
+            let html = `<div class="drama" id="${serial.id}">
+            <h2 class="title"> ${serial.name}</h2>
+            <p> <b>First episode :</b> ${serial.first_air_date} </p>
+            <a href =""> <img src="https://image.tmdb.org/t/p/w200${serial.poster_path}"></a>
+            </div>`
             let content = document.getElementById("serialData")
             content.insertAdjacentHTML("beforeend", html)
         })
